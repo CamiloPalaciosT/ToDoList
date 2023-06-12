97d5e7201cae00fd0a795a1f4f768e395bb631a7
Camilo Palacios <124638638+CamiloPalaciosT@users.noreply.github.com>
2023-06-12 18:44:00 -0500

    terminado, con módulos y buenas practicas

---
+++ script.js
@@ -0,0 +1,34 @@
+import checkComplete from "./components/checkcomplete.js"
+import deleteIcon from "./components/deleteIcon.js"
+const btn = document.querySelector('[data-form-btn]');
+
+const createTask = (evento) => {
+  evento.preventDefault();
+  const input = document.querySelector('[data-form-input]');
+  const value = input.value;
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement('li');
+  task.classList.add("card");
+  input.value = "";
+  const taskContent = document.createElement("div");
+  taskContent.appendChild(checkComplete());
+
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
+  titleTask.innerText = value;
+  taskContent.appendChild(titleTask);
+//task.innerHTML = content;
+task.appendChild(taskContent);
+task.appendChild(deleteIcon());
+list.appendChild(task);
+
+
+};
+
+
+//Arrow functions o funciones anonimas
+btn.addEventListener('click', createTask);
+
+
+
+
